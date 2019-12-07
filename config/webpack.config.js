25187246975c41831f5bfd3efb673b756a1dcf69
swapnil <ddevil691@gmail.com>
2019-12-07 19:06:22 +0530

    CSS module config changed for component scoped styles

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -435,6 +435,8 @@ module.exports = function(webpackEnv) {
               use: getStyleLoaders({
                 importLoaders: 1,
                 sourceMap: isEnvProduction && shouldUseSourceMap,
+                modules: true,
+                localIdentName: '[name]_[local]_[hash:base64:5]'
               }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
