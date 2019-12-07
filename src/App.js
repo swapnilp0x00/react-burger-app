d7f70d2100b39572fe5e16c5f95fe56a71f1014e
swapnil <ddevil691@gmail.com>
2019-12-08 01:52:41 +0530

    Added layout and css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
-    <div>
-      <Layout>
-        AA
-      </Layout>  
-    </div>
+    <Layout>
+      <BurgerBuilder></BurgerBuilder>
+    </Layout>  
   );
 }
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import classes from './Layout.css';
 import Aux from '../../HOC/Aux';
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>,
-        <main>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
