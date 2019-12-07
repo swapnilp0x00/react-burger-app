776836ceb5a726dd46bd5227913a16aca95e369e
swapnil <ddevil691@gmail.com>
2019-12-07 21:20:29 +0530

    layouting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <div>
-      App
+      <Layout>
+        AA
+      </Layout>  
     </div>
   );
 }
