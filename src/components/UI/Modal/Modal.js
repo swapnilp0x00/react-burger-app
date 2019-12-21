ece3c6433e9288199bcef79b4e5ea4207b6d3050
swapnil <ddevil691@gmail.com>
2019-12-21 11:27:17 +0530

    modal changes

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Aux from '../../../HOC/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => {
-  return (
-    <Aux>
-      <Backdrop show={props.show} clicked={props.modalClosed}></Backdrop>
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}
-      >
-        {props.children}
-      </div>
-    </Aux>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  componentWillUpdate() {
+    console.log('Yes');
+  }  
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}></Backdrop>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );  
+  }
+}
 
-export default modal;
+export default Modal;
