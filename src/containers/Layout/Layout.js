823290965054b08b8fa39752a27984fe6365a240
swapnil <ddevil691@gmail.com>
2020-01-08 19:29:22 +0530

    Path Adjustments

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import classes from './Layout.css';
 import Aux from '../../HOC/Aux';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
   state = {
     showSideDrawer: false
